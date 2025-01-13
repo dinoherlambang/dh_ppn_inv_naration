cd1005886611dff60d535d3c06878b81031508db
root <root@PAJ-DH>
2025-01-13 17:47:47 +0700

    PPn Tax naration

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# dh_ppn_inv_naration
+# PPN Invoice Narration Module
+
+This Odoo module adds a standardized PPN (Value Added Tax) narration text to invoice reports based on PMK 131/2024 regulations.
+
+## Features
+
+- Adds automatic PPN 11% narration text to invoice reports
+- Implements PMK Nomor 131 Tahun 2024 compliance text
+- Inherits and extends standard Odoo invoice report template
+
+## Technical Details
+
+The module extends `account.report_invoice_document` template by adding a fixed narration text before the existing narration field
+created by Dino Herlambang
--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,25 @@
 {
-    'name': 'Custom Invoice Report',
+    'name': 'Custom Invoice Report PPN',
     'version': '13.0.1.0.0',
+    'summary': 'Add PPN narration text to invoice reports',
+    'description': '''
+        This module adds standardized PPN narration text to invoice reports
+        based on PMK 131/2024 regulations.
+        Features:
+        - Adds automatic PPN 11% narration
+        - Implements PMK compliance text
+    ''',
+    'category': 'Accounting',
+    'author': 'Dino Herlambang',
+    'website': 'https://github.com/dinoherlambang',
+    'license': 'LGPL-3',
     'depends': ['account'],
     'data': [
         'views/report_invoice_document_inherit.xml',
     ],
+    'images': [],
+    'demo': [],
     'installable': True,
     'application': False,
+    'auto_install': False,
 }
