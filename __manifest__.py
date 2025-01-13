d72c9c5ef4c7f5a7094ab16c6023f23cbf6eb1e3
root <root@PAJ-DH>
2025-01-13 17:34:27 +0700

    added

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Custom Invoice Report',
+    'version': '13.0.1.0.0',
+    'depends': ['account'],
+    'data': [
+        'views/report_invoice_document_inherit.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
